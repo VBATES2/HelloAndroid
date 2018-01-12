8bece2017e45dc262cb89379628c7040562d2464
Victor Bates <vlbates.2@gmail.com>
2018-01-12 01:25:35 -0600

    Add files via upload

--- src/main/java/helloandroid/udacity_grow_with_google/com/MainActivity.java
+++ src/main/java/helloandroid/udacity_grow_with_google/com/MainActivity.java
@@ -2,12 +2,20 @@ package helloandroid.udacity_grow_with_google.com;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
+
     }
 }
